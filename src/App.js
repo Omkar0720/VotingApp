be3ac70cc09d9c76b511557290ce92910c750fc5
Omkar0720 <omkardeshpande201@gmail.com>
2022-09-17 15:21:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      Subjects : [
+        {name : "Cloud Computing", votes : 0},
+        {name : "Data Mining", votes : 0},
+        {name : "Advanced Java", votes : 0}
+      ]
+    }
+  }
+  vote(i){
+    let newSubjects = [...this.state.Subjects];
+    newSubjects[i].votes++;
+    this.setState({Subjects : newSubjects})
+  }
+
+  render(){
+    return(
+      <><h1>Vote for your elective</h1>
+      <div className='subjects'>
+        {this.state.Subjects.map((sub,i) => (
+          <div key={i} className="subject">
+          <div className='voteCount'>{sub.votes}</div>
+          <div className='subjectName'>{sub.name}</div>
+          <button  onClick={this.vote.bind(this,i)}>Click</button>
+          </div>
+        ))}
+
+      </div></>
+    )
+  }
 }
 
 export default App;
